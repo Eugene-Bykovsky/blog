f2ef1ac6848e4182f0fb807c0548dcd630013c0c
Evgenii Bykovskii <45427494+Eugene-Bykovsky@users.noreply.github.com>
2023-11-23 13:50:00 +0400

    change Post model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
+    publish = models.DateTimeField(cefault=timezone.now)
+    created = models.DateTimeField(auto_created=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-publish']
 
     def __str__(self):
         return self.title
